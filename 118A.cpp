b66e857538705fea7991cf362ae8afdc4c907673
Vipin Karthic <143083087+vipinkarthic@users.noreply.github.com>
2023-09-11 17:39:47 +0530

    Create 118A.cpp

---
+++ 118A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll              long long
+using namespace std;
+int main()
+{
+
+    ll n;
+    string s,t;
+    cin>>s;
+    n = s.length();
+    transform(s.begin(),s.end(),s.begin(), ::tolower);
+
+    for(ll i=0; i<n; i++){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y')
+            continue;
+        else{
+            t += '.';
+            t += s[i];
+        }
+    }
+    cout<<t<<endl;
+
+    return 0;
+}
