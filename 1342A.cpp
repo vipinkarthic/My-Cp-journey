1dbbb4a86c82ec27eef45311e87742087f9bae59
Vipin Karthic <143083087+vipinkarthic@users.noreply.github.com>
2023-09-11 18:27:16 +0530

    Create 1342A.cpp

---
+++ 1342A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll              long long
+using namespace std;
+int main()
+{
+    int t;
+    ll x,y,a,b;
+    cin>>t;
+    while(t--){
+        cin>>x>>y>>a>>b;
+
+        if(x > y){
+            swap(x,y);
+        }
+        ll ans1 = 0, ans2 = 0;
+        ans1 = x*a + y*a;
+        ll z = y - x;
+        ans2 = z*a + x * b;
+        cout<<min(ans1,ans2)<<endl;
+
+    }
+
+    return 0;
+}
